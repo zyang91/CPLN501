5d820d360b9f67743e4c1be05a09d0c467ad860e
zyang91 <zhanchao@upenn.edu>
2024-10-14 11:32:12 -0400

    cont

--- lecture_note/multiple_regression.R
+++ lecture_note/multiple_regression.R
@@ -5,3 +5,17 @@
 #Modal 3 -> log -> pop(predicted)= b+ a*log(pop-5 years ago)+ a1*year 
 # -> multiple regression
 
+# CROSS-SECTIONAL POPULATION PROJECTION
+# Assumptions of OLS regression (ordinary least square)
+#1. Homoscedasticity: residuals should be look like a random array of dots 
+# evenly dispersed around zero across the predicted values -> residuals vs. fitted values pl
+#Good: residuals are evenly distributed around the zero line
+#Bad: funnel shaped, Non-linearity
+
+reg1 <- lm(pop_plus10 ~ pop, dat_for_model)
+summary(reg1)
+abline(reg1, col = "red")
+
+# Good to close to zero, evenly distributed around the zero line
+plot(reg1)
+hist(resid(reg1))
