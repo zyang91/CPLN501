e5fc99584cbce49f3bec53ac2671a438923861d8
zyang91 <zhanchao@upenn.edu>
2024-10-19 17:13:26 -0400

    updated

--- lecture_note/cpln501_module2_assignment_tips.R
+++ lecture_note/cpln501_module2_assignment_tips.R
@@ -2,14 +2,13 @@
 #CPLN5010 QUANTITATIVE PLANNING ANALYSIS METHODS
 #FALL 2024
 
-rm(list=ls())
-
+library(tidyverse)
 #CREATING DATA####
 #enter Philly's population between 1950 and 2010
 #into the commands below
-year <- c()
-population <- c()  
-dat <- cbind.data.frame(,)
+year <- c(1950, 1960, 1970, 1980, 1990, 2000, 2010)
+population <- c(2071605,2002512,1948609,1688210,1585577,1517550,1526006)  
+dat <- cbind.data.frame(year, population)
 
 #the assignment asks you to estimate three regression models
 #1. linear
@@ -19,41 +18,45 @@ dat <- cbind.data.frame(,)
 
 #LINEAR TREND####
 #TASK: first, let's estimate a linear trend####
-mod.1 <- lm( ~ , data = )
+mod.1 <- lm(population ~ year, data = dat)
 summary(mod.1)
 
+dat$predicted_population1 <- predict(mod.1)
 #TASK: to project population in 2020 and 2030, we can feed a new data set into the predict function
-futurepop <- data.frame() #creating new data frame
+futurepop <- data.frame(year = c(2020, 2030)) #creating new data frame
 
 #TASK: make sure the variable in the new dataset has same name as indep. var. in model
 
 #TASK: create a new variable in futurepop, then store the predicted population in the variable
 
 #TASK: visualize the regression line
-plot(, ,
-     main = "", #give plot a title
-     xlab = "", ylab = "", #add labels for x and y axes
-     col = "black", pch = 16)
-abline(mod.1)
+ggplot(dat, aes(x = year, y = population)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  labs(title = "Linear Trend Philadelphia Population (2010-2010)",
+       x = "Year",
+       y = "Population")
+
+#add regression line
 #is this a good prediction for 2020? why or why not?
 
 #NON-LINEAR TREND####
 #TASK: next, let's try a model with a quadratic term
-mod.2 <- lm( ~ , data = )
+mod.2 <- lm(population ~ year+I(year^2) , data =dat)
 summary(mod.2)
 
-#will this model likely do a good job at predicting 2020 population? 
-#TASK: plot the line
-plot(, ,
-     main = "", #give plot a title
-     xlab = "", ylab = "", #add labels for x and y axes
-     col = "black", pch = 16)
 
-#TASK: plot the points
-points()
+# Get predictions from the model
+dat$predicted_population1 <- predict(mod.2)
 
-#TASK: plot smooth line through points
-lines() 
+# Plot the original data and the fitted curve
+ggplot(dat, aes(x = year, y = population)) +
+  geom_point() +  # Original data points
+  geom_line(aes(y = predicted_population1), color = "green") +  # Fitted line
+  labs(title = "Non-linear Trend of Philadelphia Population (2010-2010)",
+       x = "Year",
+       y = "Population")
+
 
 #TASK: add another column in futurepop and store predicted population in the column
 
@@ -64,15 +67,31 @@ library(tidyverse)
 dat <- dat %>% mutate(pop_lag = lag(population, 1))
 
 #TASK: build the model with year and lag
-mod.3 <- 
-summary()
+mod.3 <- lm(population ~ year + pop_lag, data = dat)
+summary(mod.3)
 
-#TASK: plot the points
-plot()
-points()
+dat$predicted_population <- NA  # Initialize the predicted column with NA values
+dat$predicted_population[!is.na(dat$year) & !is.na(dat$pop_lag)] <- predict(mod.3)
 
-#TASK: plot the line that connects the predicted values
-lines(dat$year[dat$year > 1950], predict(mod.3), col = "red") #you need to make sure the subset of data has equal number of observations for indep. adn dep. variables
+# Plot the actual data and the fitted line from mod.3
+ggplot(dat, aes(x = year, y = population)) +
+  geom_point() +  # Original data points
+  geom_line(aes(y = predicted_population), color = "red") +  # Fitted line
+  labs(title = "Lag Model of Philadelphia Population (2010-2010)",
+       x = "Year",
+       y = "Population")
+
+  
+  
+ggplot(dat, aes(x = year, y = population)) +
+  geom_point() +  # Original data points
+  geom_smooth(method = "lm", se = FALSE, color="blue") +
+  geom_line(aes(y = predicted_population1), color = "green") +  # Fitted line
+  geom_line(aes(y = predicted_population), color = "red") +  # Fitted line
+  labs(title = "Comparison of Linear, Non-linear, and Lag Models of Philadelphia Population (2010-2010)",
+       x = "Year",
+       y = "Population")+
+
 
 #TASK: project population 2020
 
